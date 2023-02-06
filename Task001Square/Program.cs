0c269bb45b3ca1961c50c2f5d4cb4965c2bae95e
NataVasilevskaya <nata.vasilevskaya11@yandex.ru>
2023-02-06 23:12:53 +0300

    Task1 file added

---
+++ Task001Square/Program.cs
@@ -0,0 +1,12 @@
+﻿// Программа принимает на вход число, возводит его в квадрат и выводит в терминал
+
+int SqureOfNum(int n)
+{
+    int square = n * n;
+    return square;
+}
+
+Console.WriteLine("Input number ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"Square of {num} is {SqureOfNum(num)}");
