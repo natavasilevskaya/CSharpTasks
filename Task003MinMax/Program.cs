f00aadd9c1493af54964229e3c2ee737afb6d642
NataVasilevskaya <nata.vasilevskaya11@yandex.ru>
2023-02-07 02:37:06 +0300

    Task3 file added

---
+++ Task003MinMax/Program.cs
@@ -0,0 +1,16 @@
+﻿// Программа принимает на вход 2 числа и выводит в консоль какое число больше и какое меньше
+
+void MinMaxNum (int n1, int n2)
+{
+    if (n1 > n2) Console.WriteLine($"{n1} bigger then {n2}");
+    if (n1 < n2) Console.WriteLine($"{n2} bigger then {n1}");
+    else Console.WriteLine($"{n1} is equal {n2}");
+}
+
+
+Console.WriteLine("Input number 1 ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input number 2 ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+MinMaxNum(num1, num2);
