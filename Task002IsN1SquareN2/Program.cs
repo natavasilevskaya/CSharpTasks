1a13a56726fdc54cc7c785a9928a47be5eba520a
NataVasilevskaya <nata.vasilevskaya11@yandex.ru>
2023-02-07 01:16:55 +0300

    Task2 file added

---
+++ Task002IsN1SquareN2/Program.cs
@@ -0,0 +1,19 @@
+﻿// Программа принимает на вход 2 числа и проверяет, является ли 1-е квадратом 2-го
+
+bool IsSquareNum (int a, int b)
+{
+    int square = b * b;
+    if(a == square)
+    {
+        return true;
+    }
+    return false;
+}
+
+Console.WriteLine("Input number 1 ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Input number 2 ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(IsSquareNum(num1, num2));
